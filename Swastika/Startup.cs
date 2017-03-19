30d9d39dd67653568f44d761e3ca3ab1dea23e8a
tintintin89 <nhathoang989@hotmail.com>
2017-03-20 00:29:17 +0700

    Fix Deploy to IIS

--- Swastika/Startup.cs
+++ Swastika/Startup.cs
@@ -41,7 +41,7 @@ namespace Swastika
         {
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DevCnn")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
